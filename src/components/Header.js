9964cc0e6cdf270aff3205582c7abd7d1085b587
Eivind Madsen Pham <eivindmpham@gmail.com>
2020-01-27 21:17:16 +0100

    Header items left and right placement

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,15 +22,18 @@ const Header = ( props ) => {
       <MdMenu className='burger-menu edit-icons' size='32'onClick={ () => setShow(!show)} />
       <header  className={ show ? 'visible' : ''} onClick={ () => setShow(false)}>
         <Link to='/home'><h1>eivind | pham</h1></Link>
-        <Link getProps={isPartiallyActive} to='/projects'>projects</Link>
-        <Link to='/about'>about</Link>
-        <Link to='/login'>
-          {
-            props.signedIn
-            ? 'profile'
-            : 'login'
-          }
-        </Link>
+        <div className='header-right-items'>
+          <Link getProps={isPartiallyActive} to='/projects'>projects</Link>
+          <div>|</div>
+          <Link to='/about'>about</Link>
+          <Link to='/login'>
+            {
+              props.signedIn
+              ? 'profile'
+              : 'login'
+            }
+          </Link>
+        </div>
       </header>
     </div>
   )
